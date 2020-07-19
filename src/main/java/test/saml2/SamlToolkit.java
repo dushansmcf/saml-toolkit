c7548a413c5cdf08993fe67ce2d944a749e3f4e6
Sajith Silva <sajiths@pagero.com>
2020-07-19 23:20:38 +0530

    Fixed a bug, added encoder

--- src/main/java/test/saml2/SamlToolkit.java
+++ src/main/java/test/saml2/SamlToolkit.java
@@ -1,8 +1,14 @@
 package test.saml2;
 
-import java.io.UnsupportedEncodingException;
+import org.bouncycastle.mime.encoding.Base64OutputStream;
+
+import javax.crypto.CipherInputStream;
+import java.io.*;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Base64;
+import java.util.zip.Deflater;
+import java.util.zip.DeflaterOutputStream;
 
 public class SamlToolkit {
     public static String decodeSAML_redirect(String payload) throws Exception {
@@ -11,6 +17,21 @@ public class SamlToolkit {
     }
 
     public static String decodeSAML_POST(String payload) throws Exception {
-        return new String(Base64.getDecoder().decode(payload), "utf-8");
+        byte[] data = Base64.getDecoder().decode(URLDecoder.decode(payload, "utf-8"));
+        return new String(data, "utf-8");
+    }
+
+    public static String encodeSAML_redirect(String xml) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        Deflater deflater = new Deflater(Deflater.DEFLATED, true);
+        DeflaterOutputStream dos = new DeflaterOutputStream(bos, deflater);
+        dos.write(xml.getBytes());
+        dos.finish();
+        byte[] b64 = Base64.getEncoder().encode(bos.toByteArray());
+        return URLEncoder.encode(new String(b64), "utf-8");
+    }
+
+    public static String encodeSAML_post(String xml) throws IOException {
+        return URLEncoder.encode(Base64.getEncoder().encodeToString(xml.getBytes()), "utf-8");
     }
 }
